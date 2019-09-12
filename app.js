ad10d1c4531f5e59bc2dbe5ed153a2b89aaecea2
Ben Reilly <ben8699@gmail.com>
2019-09-12 20:09:28 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const Promise = require('bluebird')
+const cmd = require('node-cmd')
+ 
+const getAsync = Promise.promisify(cmd.get, { multiArgs: true, context: cmd })
+
+getAsync('whoami').then(data => {
+    try {
+        const originalString = data[0]
+        var loggedUser = originalString.split("\\")
+        console.log(loggedUser[1])
+    } catch(err) {
+        console.log('cmd err', err.stack)
+    }
+}).catch(err => {
+    console.log('cmd err', err)
+})
